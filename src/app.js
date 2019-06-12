f8a392eee67ffe7bf643f9c3c91e6e3098c941f9
Adriano Biolchi <adrianobiolchi@outlook.com>
2019-06-11 21:41:50 -0300

    Setup for react + webpack

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+
+var createReactClass = require('create-react-class')
+
+const App = createReactClass({
+  render: function () {
+    return <h1>Aplicação com WebPack</h1>
+  }
+})
+
+export default App
